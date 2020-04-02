d988ae896d81f5ef6d2cd85b6c791d79d36da364
KESA24 <nekepatri@gmail.com>
2020-04-02 23:46:55 +0300

    grading programme

---
+++ grading.js
@@ -0,0 +1,17 @@
+let grade = 40;
+
+if (grade >=80){
+    console.log("You got an A");
+}
+else if(grade >= 65 && grade < 80){
+    console.log("You got a B");
+}
+else if(grade >= 50 && grade < 65){
+    console.log("You passed");
+}
+else if (grade < 50){
+    console.log("You failed the test");
+}
+else{
+    console.log("Check input");
+}
